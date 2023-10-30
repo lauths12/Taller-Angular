6db42bd5362c4987db0aac5656b0afed4e60e6b6
lauths12 <l.carretero@uniandes.edu.co>
2023-10-29 23:12:00 -0500

    COMMIT FINAL

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Taller_Angular'`, () => {
+  it(`should have as title 'tvseries'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Taller_Angular');
+    expect(app.title).toEqual('tvseries');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Taller_Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tvseries app is running!');
   });
 });
